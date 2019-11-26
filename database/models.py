9363fb153f42c136106523e0d9068ecd71654405
AleksandraOrzechowska <aorzechowska_999@wp.pl>
2019-11-26 22:45:42 +0100

    Adding classes

--- database/models.py
+++ database/models.py
@@ -2,11 +2,32 @@ from django.db import models
 
 class Suppliers(models.Model):
 	
-	CompanyName = models.CharField(max_length=200)
-	ContactName = models.CharField(max_length=200)
-	ContactTitle = models.CharField(max_length=20)
-	City = models.CharField(max_length=100)
-	Region = models.CharField(max_length=200)	
-	Country = models.CharField(max_length=20)
+	supplier_id = models.AutoField(primary_key=True)
+	company_name = models.CharField(max_length=50)
+	contact_name = models.CharField(max_length=50)
+	contact_title = models.CharField(max_length=40)
+	city = models.CharField(max_length=50)
+	region = models.CharField(max_length=40)	
+	country = models.CharField(max_length=20)
 
+class Category(models.Model):
+	category_id = models.AutoField(primary_key=True)
+	category_name = models.CharField(max_length=50)
+	description = models.TextField()
+	picture = models.BinaryField()
+
+class Products(models.Model):
+	product_id = models.AutoField(primary_key=True)
+	supplier_id = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
+	caregory_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+	quantity_per_unit = models.CharField(max_length=50)
+	unit_price = models.DecimalField(max_digits=19, decimal_places=4)
+	units_in_stock = models.SmallIntegerField()
+	units_on_order = models.SmallIntegerField()
+	reorder_level = models.SmallIntegerField()
+
+class Region(models.Model):
+	region_id = models.AutoField(primary_key=True)
+	category_name = models.CharField(max_length=50)
+	region_description = models.TextField()
 
