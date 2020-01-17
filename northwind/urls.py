1e7f7a5f5feda9b29f33d002c514297edcff4f37
korneliakli <35618023+korneliakli@users.noreply.github.com>
2020-01-17 03:33:49 +0100

    Update urls.py

--- northwind/urls.py
+++ northwind/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('northwind.urls')),
+    path('', include('database.urls')),
     path('orders/', include('orders.urls')),
 ]
